ae2a6650d2375e61d54b86255f951e62ff016d17
Bob <151000427+WilkBob@users.noreply.github.com>
2024-03-31 22:53:47 -0700

    Moved Logic and fixed initial white BG

--- src/components/Waves.js
+++ src/components/Waves.js
@@ -1,6 +1,21 @@
 import React from 'react'
-
+import { useEffect } from 'react';
 const Waves = () => {
+    useEffect(() => {
+        const body = document.body;
+    
+        const handleMouseMove = (event) => {
+          const clientX = event.clientX;
+          const hue = Math.round((clientX / window.innerWidth) * 120) + 200;
+          body.style.backgroundColor = `hsl(${hue}, 20%, 50%)`;
+        };
+    
+        body.addEventListener('mousemove', handleMouseMove);
+    
+        return () => {
+          body.removeEventListener('mousemove', handleMouseMove);
+        };
+      }, []);
   return (
     <div id='waves'>
         <div className='wave'></div>
