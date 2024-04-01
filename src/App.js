b27a62db6992ca0891259967f7bdbdb935236faa
Bob <151000427+WilkBob@users.noreply.github.com>
2024-03-31 22:46:10 -0700

    Added Cool waves and background color effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import { useEffect } from 'react';
 import LoginForm from './components/LoginForm';
 import Navbar from './components/Navbar';
+import Waves from './components/Waves';
 function App() {
+  useEffect(() => {
+    const body = document.body;
+
+    const handleMouseMove = (event) => {
+      const clientX = event.clientX;
+      const hue = Math.round((clientX / window.innerWidth) * 120) + 200;
+      body.style.backgroundColor = `hsl(${hue}, 20%, 50%)`;
+    };
+
+    body.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      body.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
   return (
     <div className="App">
       <Navbar/>
       <LoginForm/>
-      W
+      <Waves/>
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,6 +3,8 @@ import React from 'react'
 const LoginForm = () => {
   return (
     <form id='LoginForm'>
+        <h3 id='LogIn'>Log In</h3>
+        <hr></hr>
         <label>Username</label>
         <input id='username' name='username' placeholder='Enter Your Username'></input>
         <label>Password</label>
